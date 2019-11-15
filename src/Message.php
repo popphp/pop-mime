a1f76f4aa43c6f23de558c63c224ef81b9550b6b
Nick Sagona <nick@nolainteractive.com>
2019-11-14 23:10:35 -0600

    Work on createForm func

--- src/Message.php
+++ src/Message.php
@@ -114,11 +114,10 @@ class Message extends Part
      * Create multipart form object
      *
      * @param  array  $fields
-     * @param  array  $files
      * @param  string $encoding
      * @return Message
      */
-    public static function createForm($fields = [], $files = [], $encoding = null)
+    public static function createForm($fields = [], $encoding = null)
     {
         $message = new self();
         $header  = new Header('Content-Type', 'multipart/form-data', ['boundary' => $message->generateBoundary()]);
@@ -127,47 +126,59 @@ class Message extends Part
         if (!empty($fields)) {
             foreach ($fields as $name => $value) {
                 if (is_array($value)) {
-                    foreach ($value as $val) {
-                        $fieldPart = new Part(new Header('Content-Disposition', 'form-data', ['name' => $name . '[]']));
-                        $fieldPart->setBody(new Body($val, Body::RAW_URL));
+                    // Is file
+                    if (isset($value['filename'])) {
+                        $parameters   = ['name' => $name];
+                        $contentType  = null;
+                        $fileContents = null;
+
+                        foreach ($value as $key => $val) {
+                            $key = strtolower($key);
+                            if ($key == 'filename') {
+                                $parameters['filename'] = basename($val);
+                            }
+                            if (($key == 'content-type') || ($key == 'contenttype') ||
+                                ($key == 'mime-type') || ($key == 'mimetype') || ($key == 'mime')) {
+                                $contentType = $val;
+                            }
+                        }
+
+                        if (isset($value['contents'])) {
+                            $fileContents = $value['contents'];
+                        } else if (file_exists($value['filename'])) {
+                            $fileContents = file_get_contents($value['filename']);
+                        }
+
+                        $fieldPart = new Part(new Header('Content-Disposition', 'form-data', $parameters));
+                        if (null !== $contentType) {
+                            $fieldPart->addHeader('Content-Type', $contentType);
+                        }
+                        $body = new Body($fileContents);
+                        if (null !== $encoding) {
+                            $body->setEncoding($encoding)
+                                ->setSplit(76);
+                            if ($body->isBase64Encoding()) {
+                                $fieldPart->addHeader('Content-Transfer-Encoding', 'base64');
+                            } else if ($body->isQuotedEncoding()) {
+                                $fieldPart->addHeader('Content-Transfer-Encoding', 'quoted-printable');
+                            }
+                        }
+                        $fieldPart->setBody($body);
                         $message->addPart($fieldPart);
+                    } else {
+                        foreach ($value as $val) {
+                            $fieldPart = new Part(
+                                new Header('Content-Disposition', 'form-data', ['name' => $name . '[]'])
+                            );
+                            $fieldPart->setBody(new Body($val, Body::RAW_URL));
+                            $message->addPart($fieldPart);
+                        }
                     }
                 } else {
                     $fieldPart = new Part(new Header('Content-Disposition', 'form-data', ['name' => $name]));
                     $fieldPart->setBody(new Body($value, Body::RAW_URL));
                     $message->addPart($fieldPart);
                 }
-            }
-        }
-
-        if (!empty($files)) {
-            foreach ($files as $name => $value) {
-                $parameters   = ['name' => $name];
-                $contentType  = null;
-                $fileContents = null;
-
-                foreach ($value as $key => $val) {
-                    $key = strtolower($key);
-                    if ($key == 'filename') {
-                        $parameters['filename'] = basename($val);
-                    }
-                    if (($key == 'contenttype') || ($key == 'content-type') || ($key == 'mime-type') || ($key == 'mime')) {
-                        $contentType = $val;
-                    }
-                }
-
-                if (isset($value['contents'])) {
-                    $fileContents = $value['contents'];
-                } else if (file_exists($value['filename'])) {
-                    $fileContents = file_get_contents($value['filename']);
-                }
-
-                $fieldPart = new Part(new Header('Content-Disposition', 'form-data', $parameters));
-                if (null !== $contentType) {
-                    $fieldPart->addHeader('Content-Type', $contentType);
-                }
-                $fieldPart->setBody(new Body($fileContents, $encoding));
-                $message->addPart($fieldPart);
             }
         }
 
